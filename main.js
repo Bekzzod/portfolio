708f02aa30ee7dcb0fcf9ffe79a18f55081187ac
Bekzod <bek.abakulov@gmail.com>
2019-11-07 17:01:32 +0500

    Base added to ParseInt

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const burger = document.querySelector(".burger");
   if (count === words.length) {
     count = 0;
   }
-  currentWord = words[parseInt(count)];
+  currentWord = words[parseInt(count, 10)];
 
   if (!isDeleting) {
     letter = currentWord.slice(0, ++index);
